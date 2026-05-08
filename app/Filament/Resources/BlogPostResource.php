dfa404eb5183793a25be84df71cb7e062d3f37b8
Edib Zahirović <edib.zahirovic@betastudio.ba>
2026-05-08 14:13:41 +0200

    cc

--- app/Filament/Resources/BlogPostResource.php
+++ app/Filament/Resources/BlogPostResource.php
@@ -14,7 +14,7 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
-use Filament\Forms\Get;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Tables\Columns\IconColumn;
