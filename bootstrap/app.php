49b92e5bf2aa1ce8acae71cdac137d6bf866664b
Edib Zahirović <edib.zahirovic@betastudio.ba>
2026-05-19 14:41:17 +0200

    sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -28,5 +29,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('activitylog:clean')->weekly();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
