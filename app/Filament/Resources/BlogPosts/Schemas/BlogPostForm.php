57dab0f30727021e2a1de4ae661082d20570b0c4
Edib Zahirović <edib.zahirovic@betastudio.ba>
2026-05-23 17:05:11 +0200

    cc

--- app/Filament/Resources/BlogPosts/Schemas/BlogPostForm.php
+++ app/Filament/Resources/BlogPosts/Schemas/BlogPostForm.php
@@ -2,12 +2,14 @@
 
 namespace App\Filament\Resources\BlogPosts\Schemas;
 
+use App\Models\BlogAuthor;
 use Filament\Forms\Components\Builder;
 use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
@@ -177,18 +179,13 @@ class BlogPostForm
                 Section::make('Autor')
                     ->columnSpan(4)
                     ->schema([
-                        TextInput::make('author_name')
-                            ->label('Ime autora')
+                        Select::make('blog_author_id')
+                            ->label('Autor')
+                            ->relationship('author', 'name')
+                            ->searchable()
+                            ->preload()
                             ->required()
-                            ->maxLength(120),
-
-                        FileUpload::make('author_avatar')
-                            ->label('Avatar autora')
-                            ->image()
-                            ->avatar()
-                            ->disk('r2')
-                            ->directory('blog/authors')
-                            ->circleCropper(),
+                            ->helperText('Autori se upravljaju na stranici Autori.'),
                     ]),
             ]);
     }
